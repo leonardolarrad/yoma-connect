dd692bd1889e58b4ceec9e67586c7ca60eeeb17c
Leonardo Larrad <leonardolarrad@gmail.com>
2026-07-18 15:05:08 -0400

    Yoma Connect

---
+++ include/yoma-connect.hh
@@ -0,0 +1,3 @@
+#pragma once
+
+// Placeholder for future yoma-connect headers
---
+++ src/main.cc
@@ -0,0 +1,7 @@
+#include "yoma-connect.hh"
+#include <print>
+
+int main() {
+  std::println("Hello from yoma-connect with C++23!");
+  return 0;
+}
